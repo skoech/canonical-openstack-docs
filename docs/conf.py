8f7fc1d8584415083b824bc5f9305cb001038e7e
Sharon Koech <sharon.koech@canonical.com>
2026-09-04 12:36:27 +0300

    fix: remove _static from _dev, add templates_path
    Signed-off-by: Sharon Koech <sharon.koech@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ sitemap_excludes = [
 ################################
 
 html_static_path = ["_static"]
-# templates_path = ["_templates"]
+templates_path = ["_templates"]
 
 #############
 # Redirects #
